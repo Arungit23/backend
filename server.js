a2754eb98bb3e19d4817d56d7846964442aa06c6
Arunkumar k <122257923+Arungit23@users.noreply.github.com>
2024-03-14 02:05:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://myinventory-gules.vercel.app"],
+    origin: ["http://localhost:3000", "https://my-inventry.vercel.app/"],
     credentials: true
 }));
 
