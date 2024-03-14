d867a1b12b8fe3689552d4e6b8c54426723313d8
Arunkumar k <122257923+Arungit23@users.noreply.github.com>
2024-03-14 08:47:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://my-inventry.vercel.app/"],
+    origin: ["http://localhost:3000", "https://my-inventry-theta.vercel.app/"],
     credentials: true
 }));
 
