2468fa4e570780f31eea509eb5b8d375b8ee08ca
Arunkumar k <122257923+Arungit23@users.noreply.github.com>
2024-03-14 09:04:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://inven-troy.vercel.app/"],
+    origin: ["http://localhost:3000", "https://innven-try.vercel.app"],
     credentials: true
 }));
 
